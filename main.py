50796b15468e5565e96587d49ae5fc8b97f98c58
Karl-Philippe Beaudet <kp.beaudet@gmail.com>
2024-12-28 16:22:56 -0500

    Anonymized main

--- main.py
+++ main.py
@@ -1,31 +1,22 @@
 import os
+import json
 from datetime import date
 from src.models import Person
 from src.utils import clear_directory, encode_image
 from src.santa import create_secret_santa, check_assignments
 
-# Define participants and configuration at the beginning
-participants = [
-    Person("Karl-Philippe", "Famille A", "male"),
-    Person("Chloé", "Famille A", "female"),
-    Person("Léa", "Famille A", "female"),
-    Person("Sylvain", "Famille A", "male"),
-    Person("Sylvie", "Famille A", "female"),
-    Person("Jocelyne", "Famille B", "female"),
-    Person("Sasha", "Famille B", "male"),
-    Person("Frédéric", "Famille B", "male"),
-    Person("Malou", "Famille B", "female"),
-    Person("Félix", "Famille B", "male"),
-    Person("Julian", "Famille B", "male"),
-    Person("Lucie", "Famille C", "female"),
-    Person("Louis", "Famille C", "male"),
-]
+# Load participants from parameter file
+parameter_file = "participants.json"
+with open(parameter_file, "r", encoding="utf-8") as file:
+    participant_data = json.load(file)
+
+participants = [Person(p["name"], p["family"], p["gender"]) for p in participant_data]
 
 # Configuration settings
 directory = os.path.join("results", f"Échange_cadeau_{date.today().year}")
 template_path = os.path.join("templates", "instruction_template.html")
 max_budget = 40
-max_intra_family = 1
+max_intra_family = 0
 
 # Function to generate HTML instruction files
 def generate_instruction_files(assigned_participants, directory, template_path, max_budget):
